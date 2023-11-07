4953d2d98459c1e24313eaff30325fbd86af81f9
oanastassov <75079980+oanastassov@users.noreply.github.com>
2023-11-07 13:43:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function changeHeadshotImg1(){
+    document.getElementById("headshotimg").src = "images/kayak_bailey.jpg";
+    document.getElementById("aboutTextHeadshot").textContent = "The Supermax Rapid in Bailey Canyon, CO (Class V)";
+};
+function changeHeadshotImg2(){
+    document.getElementById("headshotimg").src = "images/headshot.jpg";
+    document.getElementById("aboutTextHeadshot").textContent = "";
+};
+  
